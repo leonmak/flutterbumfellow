2a928b48c08e0998467ff676ea4ac1657310c3c5
leon mak <leonmakansheng@gmail.com>
2015-12-06 13:32:29 +0800

    Added todos checklist w/archive btn

---
+++ client/templates/todos/todos.js
@@ -0,0 +1,33 @@
+
+Template.todos.helpers({
+  todos: function(){
+     return Todos.find({project:Session.get('active_project'),archived:{$ne:true}});
+  },
+    adding_todo: function(){
+    return Session.get('adding_todo');
+}
+});
+
+Template.todos.events({
+  "click .archive": function(event, template){
+    return Meteor.call('archiveTodo',this._id,!this.archived);
+  },
+  'click .todochecked':function(evt,tmpl){
+    return Meteor.call('completeTodo',this._id,!this.completed);
+  },
+  'click .addtodo':function(evt,tmpl){
+    Session.set('adding_todo',true);
+  }
+});
+Template.todoDlg.events({
+  'click .saveTodo':function(evt,tmpl){
+    var todo = {};
+    todo.note = tmpl.find('.todoitem').value;
+    todo.project = Session.get('active_project');
+    Meteor.call('addTodo',todo);
+    Session.set('adding_todo',false);
+  },
+  'click .closeTodo':function(evt,tmpl){
+    Session.set('adding_todo',false);
+  }
+})
--- server/email/config.js
+++ server/email/config.js
@@ -70,6 +70,19 @@ Meteor.startup(function() {
         'archiveConversation':function(id,archived){
             Conversations.update({_id:id},{$set:{archived:archived}});
         },
+        'archiveTodo':function(id,archived){
+            Todos.update({_id:id},{$set:{archived:archived}});
+        },
+        'completeTodo':function(id,complete){
+            Todos.update({_id:id},{$set:{completed:complete}});
+        },
+        'addTodo':function(todo){
+            todo.userId = Meteor.userId();
+            todo.dateadded = new Date();
+            todo.archived = false;
+            todo.completed = false;
+            return Todos.insert(todo);
+        },
 
     });
 });
